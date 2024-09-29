8397419fe2b56d7ec1bbf4f5c9d6cdc172865057
maiychrus25 <ninhem575@gmail.com>
2024-09-30 05:15:30 +0700

    Bai 2

--- Bai2/BienLai.java
+++ Bai2/BienLai.java
@@ -6,22 +6,23 @@ public class BienLai {
     private ChuHo chuHo;
     private int chiSoCu;
     private int chiSoMoi;
-    
-    public BienLai() {}
-    
+
+    public BienLai() {
+    }
+
     public BienLai(ChuHo chuHo, int chiSoCu, int chiSoMoi) {
         this.chuHo = chuHo;
         this.chiSoCu = chiSoCu;
         this.chiSoMoi = chiSoMoi;
     }
-    
+
     public int tinhTienNuoc() {
         return (this.chiSoMoi - this.chiSoCu) * 25000;
     }
-    
+
     @Override
     public String toString() {
-        return "Chi So Cu: " + this.chiSoCu + "\nChi So Moi: " + this.chiSoMoi
-                + "\nTien Nuoc Phai Tra: " + this.tinhTienNuoc() + " VND";
+        return "Chỉ Số Cũ: " + this.chiSoCu + "\nChỉ Số Mới: " + this.chiSoMoi
+                + "\nTiền Nước Phải Trả: " + this.tinhTienNuoc() + " VND";
     }
 }
