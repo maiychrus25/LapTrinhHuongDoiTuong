3fdfaee4dd5527f0767fe7af5689a65a12235838
maiychrus25 <ninhem575@gmail.com>
2024-09-30 00:14:54 +0700

    Bai 1

---
+++ Bai1/AgeNotEligibleException.java
@@ -0,0 +1,9 @@
+//Author: MAIY 07!!
+//Created: 29.09.2024
+package Bai1;
+
+public class AgeNotEligibleException extends Exception {
+    public AgeNotEligibleException(String message) {
+        super(message);
+    }
+}
---
+++ Bai1/InvalidEmailException.java
@@ -0,0 +1,9 @@
+//Author: MAIY 07!!
+//Created: 29.09.2024
+package Bai1;
+
+public class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
