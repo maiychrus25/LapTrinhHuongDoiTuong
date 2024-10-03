0bfddd127ce14530a11c4c8f3426425c14501d62
Nịnh Thanh Khương <165678100+maiychrus25@users.noreply.github.com>
2024-10-03 09:08:29 +0700

    Update CuocHen.java

--- Bai14/CuocHen.java
+++ Bai14/CuocHen.java
@@ -11,26 +11,26 @@ public class CuocHen {
     public CuocHen () {}
     
     public CuocHen(String maCuocHen, LocalTime thoiGianBatDau, LocalTime thoiGianKetThuc) {
-		this.maCuocHen = maCuocHen;
-		this.thoiGianBatDau = thoiGianBatDau;
-		this.thoiGianKetThuc = thoiGianKetThuc;
-	}
+	    this.maCuocHen = maCuocHen;
+    	this.thoiGianBatDau = thoiGianBatDau;
+	    this.thoiGianKetThuc = thoiGianKetThuc;
+    }
 
 	public void input(Scanner sc) {
-        System.out.print("Nhap ma cuoc hen: ");
-        this.maCuocHen = sc.nextLine();
+    System.out.print("Nhap ma cuoc hen: ");
+    this.maCuocHen = sc.nextLine();
 
-        System.out.print("Nhap thoi gian bat dau (hh:mm): ");
-        this.thoiGianBatDau = LocalTime.parse(sc.nextLine());
+    System.out.print("Nhap thoi gian bat dau (hh:mm): ");
+    this.thoiGianBatDau = LocalTime.parse(sc.nextLine());
 
-        System.out.print("Nhap thoi gian ket thuc (hh:mm): ");
-        this.thoiGianKetThuc = LocalTime.parse(sc.nextLine());
-    }
+    System.out.print("Nhap thoi gian ket thuc (hh:mm): ");
+    this.thoiGianKetThuc = LocalTime.parse(sc.nextLine());
+  }
     
     public void isValidThoiGian() throws InvalidTimeException  {
-        if (!thoiGianKetThuc.isAfter(thoiGianBatDau)) {
-            throw new InvalidTimeException ("Thoi gian ket thuc phai lon hon thoi gian bat dau!");
-        }
+      if (!thoiGianKetThuc.isAfter(thoiGianBatDau)) {
+         throw new InvalidTimeException ("Thoi gian ket thuc phai lon hon thoi gian bat dau!");
+      }
     }
     
     @Override
