1f97bb846b2d964852d2b4ccf44c21be5d4639b0
Nịnh Thanh Khương <165678100+maiychrus25@users.noreply.github.com>
2024-11-04 20:45:58 +0700

    Update InvalidYearException.java

--- BKTTH_02/Bai5/InvalidYearException.java
+++ BKTTH_02/Bai5/InvalidYearException.java
@@ -1,5 +1,12 @@
 package BKTTH_02.Bai5;
 
+/**
+*    Author:  MAIY07!!
+*    Created: Oct 31, 2024 1:27:08 PM
+**/
+
+package BKTTH_02.Bai5;
+
 @SuppressWarnings("serial")
 public class InvalidYearException extends Exception {
 	public InvalidYearException(String message) {
