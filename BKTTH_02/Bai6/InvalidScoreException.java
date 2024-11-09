f47eb9f2246232a07879b7f033820c7b0720b6e8
Nịnh Thanh Khương <165678100+maiychrus25@users.noreply.github.com>
2024-11-09 18:56:36 +0700

    Update InvalidScoreException.java

--- BKTTH_02/Bai6/InvalidScoreException.java
+++ BKTTH_02/Bai6/InvalidScoreException.java
@@ -1,4 +1,9 @@
-// package BKTTH_02.Bai6;
+/**
+*    Author:  MAIY07!!
+*    Created: Oct 31, 2024 1:27:08 PM
+**/
+
+package BKTTH_02.Bai6;
 
 @SuppressWarnings("serial")
 public class InvalidScoreException extends Exception {
